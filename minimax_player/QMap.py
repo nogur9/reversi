2b153ab51e0ed96e75bd3422fadc6e9cebf6079b
nogur9 <nogur9@gmail.com>
2018-08-30 11:14:13 +0300

    ,mnb

--- minimax_player/QMap.py
+++ minimax_player/QMap.py
@@ -1,29 +1,47 @@
 from board import Board
 from disk import Disk
 import itertools
-from abstract_player import AbstractPlayer
+import pickle
+import shelve
+
 class QMap:
-    alpha = 1
-    gamma = 0.8
-    def __init__(self):
+
+    def __init__(self, player, load=0):
+        self.learning_rate = 0.3
         product = itertools.product([Disk.NONE, Disk.LIGHT, Disk.DARK], repeat=64)
-        self.qmap = {}
-        players = [AbstractPlayer(Disk.LIGHT), AbstractPlayer(Disk.DARK)]
-        for player in players:
+        self.discount = 0.8
+        if load:
+            with open('myfile.pkl', 'rb') as handle:
+                self.qmap = pickle.load(handle)
+        else:
+            self.qmap = {}
             for iter in product:
                 board = Board(8)
                 for x in range(8):
                     for y in range(8):
-                        board[x][y] = iter[8*x+y]
+                        board.square_matrix[x][y] = iter[8*x+y]
                 for move in board.get_possible_moves(player):
-                #key is player_color, num_x, num_y, 64 colors of the board
-                    key = str(player.color.value)+str(x)+str(y)+"".join([str(sqr.color.value) for sqr in iter])
-                    self.qmap[key] = 1
-    def update (self, key, reward):
-        self.qmap[key] =
+                #key is action, state -  num_x, num_y, 64 colors of the board
+                    key = (str(move[0])+str(move[1]), "".join([str(sqr.value) for sqr in iter]))
+                    self.qmap[key] = 0
+            self.save()
 
-class VMap:
 
-    def __init__(self):
-        product = itertools.product([Disk.NONE, Disk.LIGHT, Disk.DARK], repeat=64)
-        vmap = {board: 1 for board in product}
+    def update(self, key, reward, new_state):
+        self.qmap[key] = (1 - self.learning_rate) * self.qmap[key] + self.learning_rate *\
+                         (reward + self.discount * self.max_q(new_state))
+
+
+    def max_q(self, new_state):
+        val = max([v for k, v in self.qmap.items() if new_state in k])
+        return val
+
+    def save(self, path = 'myfile.pkl'):
+        output = open(path, 'wb')
+        pickle.dump(self.qmap, output)
+        output.close()
+
+    def load(self, path = 'myfile.pkl'):
+        output = open(path, 'wb')
+        pickle.dump(self.qmap, output)
+        output.close()
